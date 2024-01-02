ee90aee506b32bae44f0f8b001d509582e749e84
Tej Chajed <tchajed@gmail.com>
2024-01-01 21:46:43 -0600

    Move classical lib out

--- src/arrow.v
+++ src/arrow.v
@@ -1,41 +1,8 @@
 From arrow Require Import options.
 
-From Coq Require Logic.Classical.
+From arrow Require classical.
 From stdpp Require Import ssreflect.
 From stdpp Require Import prelude vector.
-
-Module classical.
-  Import Coq.Logic.Classical.
-
-  Lemma not_not (P: Prop) :
-    ~~P ↔ P.
-  Proof. tauto. Qed.
-  Lemma not_or (P Q: Prop) :
-    ~(P ∨ Q) ↔ ~P ∧ ~Q.
-  Proof. tauto. Qed.
-  Lemma not_and (P Q: Prop) :
-    ~(P ∧ Q) ↔ ~P ∨ ~Q.
-  Proof. tauto. Qed.
-  Lemma not_all (A: Type) (P: A → Prop) :
-    (~∀ x, P x) ↔ ∃ x, ~P x.
-  Proof.
-    split.
-    - apply not_all_ex_not.
-    - apply ex_not_not_all.
-  Qed.
-  Lemma not_ex (A: Type) (P: A → Prop) :
-    (~∃ x, P x) ↔ (∀ x, ~P x).
-  Proof.
-    rewrite -(not_not (∀ x, ~P x)).
-    rewrite not_all.
-    setoid_rewrite not_not.
-    reflexivity.
-  Qed.
-
-  Lemma excluded_middle (P: Prop) : P ∨ ¬P.
-  Proof. apply Classical_Prop.classic. Qed.
-
-End classical.
 
 Ltac simplify_decide :=
   repeat
---
+++ src/classical.v
@@ -0,0 +1,30 @@
+From stdpp Require Import prelude.
+From Coq Require Import Logic.Classical.
+
+Lemma not_not (P: Prop) :
+  ~~P ↔ P.
+Proof. tauto. Qed.
+Lemma not_or (P Q: Prop) :
+  ~(P ∨ Q) ↔ ~P ∧ ~Q.
+Proof. tauto. Qed.
+Lemma not_and (P Q: Prop) :
+  ~(P ∧ Q) ↔ ~P ∨ ~Q.
+Proof. tauto. Qed.
+Lemma not_all (A: Type) (P: A → Prop) :
+  (~∀ x, P x) ↔ ∃ x, ~P x.
+Proof.
+  split.
+  - apply not_all_ex_not.
+  - apply ex_not_not_all.
+Qed.
+Lemma not_ex (A: Type) (P: A → Prop) :
+  (~∃ x, P x) ↔ (∀ x, ~P x).
+Proof.
+  rewrite <- (not_not (∀ x, ~P x)).
+  rewrite not_all.
+  setoid_rewrite not_not.
+  reflexivity.
+Qed.
+
+Lemma excluded_middle (P: Prop) : P ∨ ¬P.
+Proof. apply Classical_Prop.classic. Qed.
